d620701844cbe2c19256ce6b2854561a71f1d354
IraLysenko <arch_ira@ukr.net>
2022-11-11 23:15:04 +0100

    battleship: fixed columns numbers, changed background

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -16,7 +16,7 @@ export class Board extends React.Component {
         // }
     }
 
-    columns = [0,1,2,3,4,5,6,7,8,9,10];
+    columns = [1,2,3,4,5,6,7,8,9,10];
     rows = ['A','B','C','D','E','F','G','H','I','G'];
 
     renderTableHeader() {
