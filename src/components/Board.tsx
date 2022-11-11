267f4fad2382b3ec5ab1e0a92f0482b5f34cac8e
IraLysenko <arch_ira@ukr.net>
2022-11-11 22:37:37 +0100

    battleship: created board loop, changed cell size

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -15,193 +15,41 @@ export class Board extends React.Component {
         // }
     }
 
-    columnNumbers = 6;
-    rowNumbers = 6;
+    columns = [0,1,2,3,4,5,6,7,8,9,10];
+    rows = ['A','B','C','D','E','F','G','H','I','G'];
+
+    renderTableHeader() {
+        return(
+            <tr>
+                <th className="numbers"></th>
+                {this.columns.map((number, index) => {
+                    return <th key={index}>{number}</th>
+                })}
+            </tr>
+        )
+    }
+
+    renderTableData() {
+        return this.rows.map((letter, letterIndex) => {
+            return (
+                <tr>
+                    <th className="letters">{letter}</th>
+                    {this.columns.map((number, numberIndex) => {
+                        return <td>
+                            <div id={letter + '-' + number}></div>
+                        </td>
+                    })}
+                </tr>
+            )
+        })
+    }
 
     render() {
         return (
             <div id="board">
                 <table>
-
-                    <tr>
-                        <th className="numbers"></th>
-                        <th className="numbers">0</th>
-                        <th className="numbers">1</th>
-                        <th className="numbers">2</th>
-                        <th className="numbers">3</th>
-                        <th className="numbers">4</th>
-                        <th className="numbers">5</th>
-                        <th className="numbers">6</th>
-                    </tr>
-
-                    <tr>
-                        <th className="letters">A</th>
-                        <td>
-                            <div id="00"></div>
-                        </td>
-                        <td>
-                            <div id="01"></div>
-                        </td>
-                        <td>
-                            <div id="02"></div>
-                        </td>
-                        <td>
-                            <div id="03"></div>
-                        </td>
-                        <td>
-                            <div id="04"></div>
-                        </td>
-                        <td>
-                            <div id="05"></div>
-                        </td>
-                        <td>
-                            <div id="06"></div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th className="letters">B</th>
-                        <td>
-                            <div id="10"></div>
-                        </td>
-                        <td>
-                            <div id="11"></div>
-                        </td>
-                        <td>
-                            <div id="12"></div>
-                        </td>
-                        <td>
-                            <div id="13"></div>
-                        </td>
-                        <td>
-                            <div id="14"></div>
-                        </td>
-                        <td>
-                            <div id="15"></div>
-                        </td>
-                        <td>
-                            <div id="16"></div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th className="letters">C</th>
-                        <td>
-                            <div id="20"></div>
-                        </td>
-                        <td>
-                            <div id="21"></div>
-                        </td>
-                        <td>
-                            <div id="22"></div>
-                        </td>
-                        <td>
-                            <div id="23"></div>
-                        </td>
-                        <td>
-                            <div id="24"></div>
-                        </td>
-                        <td>
-                            <div id="25"></div>
-                        </td>
-                        <td>
-                            <div id="26"></div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th className="letters">D</th>
-                        <td>
-                            <div id="30"></div>
-                        </td>
-                        <td>
-                            <div id="31"></div>
-                        </td>
-                        <td>
-                            <div id="32"></div>
-                        </td>
-                        <td>
-                            <div id="33"></div>
-                        </td>
-                        <td>
-                            <div id="34"></div>
-                        </td>
-                        <td>
-                            <div id="35"></div>
-                        </td>
-                        <td>
-                            <div id="36"></div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th className="letters">E</th>
-                        <td>
-                            <div id="40"></div>
-                        </td>
-                        <td>
-                            <div id="41"></div>
-                        </td>
-                        <td>
-                            <div id="42"></div>
-                        </td>
-                        <td>
-                            <div id="43"></div>
-                        </td>
-                        <td>
-                            <div id="44"></div>
-                        </td>
-                        <td>
-                            <div id="45"></div>
-                        </td>
-                        <td>
-                            <div id="46"></div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th className="letters">F</th>
-                        <td>
-                            <div id="50"></div>
-                        </td>
-                        <td>
-                            <div id="51"></div>
-                        </td>
-                        <td>
-                            <div id="52"></div>
-                        </td>
-                        <td>
-                            <div id="53"></div>
-                        </td>
-                        <td>
-                            <div id="54"></div>
-                        </td>
-                        <td>
-                            <div id="55"></div>
-                        </td>
-                        <td>
-                            <div id="56"></div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th className="letters">G</th>
-                        <td>
-                            <div id="60"></div>
-                        </td>
-                        <td>
-                            <div id="61"></div>
-                        </td>
-                        <td>
-                            <div id="62"></div>
-                        </td>
-                        <td>
-                            <div id="63"></div>
-                        </td>
-                        <td>
-                            <div id="64"></div>
-                        </td>
-                        <td>
-                            <div id="65"></div>
-                        </td>
-                        <td>
-                            <div id="66"></div>
-                        </td>
-                    </tr>
+                    {this.renderTableHeader()}
+                    {this.renderTableData()}
                 </table>
             </div>
         )
