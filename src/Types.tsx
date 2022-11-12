40fbbdb06dd82895bf3678169047ee1a5f6176b7
IraLysenko <arch_ira@ukr.net>
2022-11-12 17:36:38 +0100

    battleship: added types file

---
+++ src/Types.tsx
@@ -0,0 +1,21 @@
+export enum ShipsTypes {
+    SUBMARINE=1,
+    DESTROYER=2,
+    CRUISER=3,
+    BATTLESHIP=4,
+    AIRCRAFTCARIER=5,
+}
+
+export enum ShipsAmount {
+    SUBMARINE=2,
+    DESTROYER=2,
+    CRUISER=1,
+    BATTLESHIP=1,
+    AIRCRAFTCARIER=1,
+}
+
+export type CellPrototype = {
+    coordinate: string;
+    dataShip: boolean;
+    //onClick: (event: React.MouseEvent<HTMLButtonElement>) => void
+}
